de12c86f106dcad42e8f94d66ad7e07674c3d8b5
blakethomas <blakekthomas49@gmail.com>
2017-09-29 12:53:10 -0700

    Fixes car element.

--- main.js
+++ main.js
@@ -3,25 +3,47 @@ class Car {
     this.direction = direction
     this.speed = speed
     this.location = location
-
-    const $carHolder = document.createElement('div')
-    const $carImage = document.createElement('img')
-    $carImage.src = 'http://downloadclipart.org/do-upload/clipart/2017-07/Car_automobile_vehicle_clipart.png'
-    $carImage.width = '150'
-    $carImage.height = '100'
-    $carImage.setAttribute('style', 'position: absolute;')
-    $carHolder.appendChild($carImage)
-    document.body.appendChild($carHolder)
+  }
+  turn(newDirection) {
+    this.direction = newDirection
+  }
+  accelerate(ammount) {
+    this.speed += ammount
+  }
+  move() {
+    switch (this.direction) {
+      case 'north':
+        this.location[1] -= this.speed
+        break
+      case 'south':
+        this.location[1] += this.speed
+        break
+      case 'east':
+        this.location[0] += this.speed
+        break
+      case 'west':
+        this.location[0] -= this.speed
+    }
   }
 }
 
+class Window {
+  constructor(width, height, car){
+
+    this.width = width
+    this.height =  height
+    this.car = camry
+  }
+}
+
+const $carHolder = document.createElement('div')
+const $carImage = document.createElement('img')
+$carImage.src = 'http://downloadclipart.org/do-upload/clipart/2017-07/Car_automobile_vehicle_clipart.png'
+$carImage.width = '150'
+$carImage.height = '100'
+$carImage.setAttribute('style', 'position: absolute;')
+$carHolder.appendChild($carImage)
+document.body.appendChild($carHolder)
+
 const camry = new Car('north', 0, '0, 0')
-
-
-const $roadLine = document.createElement('div')
-$roadLine.classList.add('roadline')
-
-
-
-
-document.body.appendChild($roadLine)
+const gameWindow = new Window(500, 500)
